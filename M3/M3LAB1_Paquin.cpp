91ddf702d6a0b96cef5b37605eec4a2994f42195
henpaq <156948224+henpaq@users.noreply.github.com>
2024-02-14 19:08:55 +0000

    M3LAB1

---
+++ M3/M3LAB1_Paquin.cpp
@@ -0,0 +1,95 @@
+// Comment
+/*
+CSC 134
+M3LAB1 - Story Game
+H Paquin
+2/14/24
+*/
+
+#include <iostream>
+using namespace std;
+
+// beginning of the main() method
+int main() {
+
+  // declare the variable we need
+  int choice; 
+
+  // ask the question
+  cout << "Do you want to drive for Red Bull (1) or Ferrari (2)?" << endl;
+  cout << "Type 1 or 2: "; 
+  cin >> choice;
+
+  // using if, make a decision based on the user's choice
+
+  if (1 == choice) {
+  	cout << "You chose Red Bull" << endl;
+    cout << endl;
+    cout << "It is the final race of the season. You are winning the championship, but there is an issue." << endl;
+    cout << "Your team warns you that there is potential rain in 5 minutes. They give you the option to box or stay out." << endl;
+    cout << endl;
+    cout << "What do you choose? (Box (1) or stay out (2))" << endl;
+    cout << endl;
+    cin >> choice;
+    if (1 == choice) {
+        cout << "You chose to box" << endl;
+        cout << endl;
+        cout << "Your team makes a perfect pit stop!" << endl;
+        cout << "The rain falls down heavily, and you are the only one on Wet tires." << endl;
+        cout << endl;
+        cout << "Congratulations! You won the Championship and the race!" << endl;
+        cout << endl;
+    }
+    if (2 == choice) {
+        cout << "You chose to stay out" << endl;
+        cout << endl;
+        cout << "The rain on the forecast is approaching!" << endl;
+        cout << "The cars behind you decide to change tires for wet tires." << endl;
+        cout << "Luckily the rain is not heavy enough and you are able to maintain your lead!" << endl;
+        cout << endl;
+        cout << "Congratulations! You won the Championship and the race!" << endl;
+        cout << endl;
+  }
+  }
+  else if (2 == choice) {
+  	cout << "You chose Ferrari" << endl;
+    cout << endl;
+    cout << "It is the final race of the season. You are winning the championship, but there is an issue." << endl;
+    cout << "Your team warns you that there is potential rain in 5 minutes. They give you the option to box or stay out." << endl;
+    cout << endl;
+    cout << "What do you choose? (Box (1) or stay out (2))" << endl;
+    cout << endl;
+    cin >> choice;
+    if (1 == choice) {
+        cout << "You chose to box" << endl;
+        cout << endl;
+        cout << "Your team calls you in, but your teammate is already in the pits!" << endl;
+        cout << "Your team tries to tell you to stay out, but it is too late." << endl;
+        cout << "Your team has ruined your pit stop!" << endl;
+        cout << "When you come out of the pits, you lost the lead, and your now losing the championship!" << endl;
+        cout << endl;
+        cout << "Sadly, you end up losing the championship" << endl;
+        cout << endl;
+    }
+     if (2 == choice) {
+        cout << "You chose to stay out" << endl;
+        cout << endl;
+        cout << "The rain on the forecast has gone away, and the race will stay dry!" << endl;
+        cout << "On lap 55 / 57, you start to feel a vibration in your car." << endl;
+        cout << "Seconds later you have a puncture!" << endl;
+        cout << "You are forced to pull over and retire the car." << endl;
+        cout << endl;
+        cout << "Sadly, you end up losing the championship" << endl;
+        cout << endl;
+    }
+  }
+  else {
+  	cout << "I'm sorry, that is not a valid choice." << endl;
+  }
+
+  // finish up
+  cout << "Thanks for playing!" << endl;
+  cout << endl; // this runs no matter what they choose
+  return 0; // exit without error
+
+} // end of the main() method
---
+++ starter.cpp
@@ -0,0 +1,22 @@
+// if statements - example 1 
+// standard header
+// (by now you should remember
+// what goes here)
+// name of the class
+// name of the assignment
+// your name
+// the date
+
+// compiler directives
+// (required for the program to run)
+#include <iostream>
+using namespace std;
+
+// beginning of the main() method
+int main() {
+
+  cout << "Hello World!" << endl; // this is as simple as a program gets
+
+  return 0; // lets computer know that we finished without errors
+
+} // end of the main() method
