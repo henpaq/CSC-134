f46d5f1138842da1c35151c796089f3de3c7a418
henpaq <156948224+henpaq@users.noreply.github.com>
2024-02-21 19:11:33 +0000

    M3T3

---
+++ M3/M3T3_Paquin.cpp
@@ -0,0 +1,48 @@
+// Comment
+/*
+CSC 134
+M3T3 - RNG
+H Paquin
+2/21/24
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main() {
+    // variables
+    int roll, die1, die2;
+    int seed;
+
+    // main code
+    cout << "Welcome to the craps table" << endl;
+    //cout << "Enter your lucky number: " << endl;
+    //cin >> seed;
+
+    // set the random seed
+    seed = time(0);
+    srand(seed);
+    
+    // roll the dice
+    die1 = (rand() % 6) + 1; // roll 0-5, add 1
+    die2 = (rand() % 6) + 1;
+    roll = die1 + die2;
+    cout << "Your dice roll is: " << die1 << " and " << die2 << " -> " << roll << endl;
+
+    // TODO: Check for win, loss, or point
+    if (roll == 4 || roll == 2) {
+        cout << "Loss" << endl;
+    }
+    else if (roll == 6 || roll == 9 || roll == 12) {
+        cout << "Win!" << endl;
+    }
+    else {
+        cout << "Your point is " << roll << endl;
+    }
+
+    return 0; 
+
+} 
