32254d9f4f0a2536d46529ca4dad66573973f492
henpaq <156948224+henpaq@users.noreply.github.com>
2024-05-01 17:58:13 +0000

    M7T2

--- M7/M7T2_Paquin.cpp
+++ M7/M7T2_Paquin.cpp
@@ -8,16 +8,22 @@ int main() {
     Rectangle r;
 
     double length, width, area;
+    string text;
     cout << "Enter length: ";
     cin >> length;
     cout << "Enter width: ";
     cin >> width;
+    cout << "Enter character or emoji: ";
+    cin >> text;
 
     r.setLength(length);
-    r.setLength(width);
+    r.setWidth(width);
+    r.setChar(text);
     cout << "Retangle is: " << endl;
     cout << r.getLength() << " by " << r.getWidth() << endl;
     cout << "Rectangle area = " << r.getArea() << endl;
+    cout << "Printing the Rectangle" << endl;
+    r.drawRectangle();
     return 0;
 
   return 0; 
--- M7/Rectangle.h
+++ M7/Rectangle.h
@@ -1,17 +1,22 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
+#include <iostream>
+using namespace std;
 
 // class declaration
 class Rectangle {
     private:
         double width;
         double length;
+        string text;
     public:
-    void setLength(double);
-    void setWidth(double);
-    double getLength() const;
-    double getWidth() const;
-    double getArea() const;
+        void setLength(double);
+        void setWidth(double);
+        void setChar(string);
+        double getLength() const;
+        double getWidth() const;
+        double getArea() const;
+        void drawRectangle() const;
 };
 
 
@@ -29,6 +34,10 @@ void Rectangle::setWidth(double w) {
     width = w;
 }
 
+void Rectangle::setChar(string ch) {
+    text = ch;
+}
+
 double Rectangle::getLength() const {
     return length;
 }
@@ -40,6 +49,15 @@ double Rectangle::getWidth() const {
 double Rectangle::getArea() const {
     double area = length * width;
     return area;
+}
+
+void Rectangle::drawRectangle() const {
+    for (int i=0; i<length; i++) {
+        for (int j=0; j<width; j++) {
+            cout << text;
+        }
+        cout << endl;
+    }
 }
 
 
